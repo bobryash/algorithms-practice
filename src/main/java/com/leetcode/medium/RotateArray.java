3b20c715ec72eeddf7fdd36c0a626e74b1e597b7
Mikhail <bobryash@gmail.com>
2023-01-12 19:21:08 +0100

    added comments to 189 leetcode

--- src/main/java/com/leetcode/medium/RotateArray.java
+++ src/main/java/com/leetcode/medium/RotateArray.java
@@ -3,6 +3,7 @@ package com.leetcode.medium;
 import java.util.Arrays;
 
 /**
+ * #189
  * Given an array, rotate the array to the right by k steps, where k is non-negative.
  *
  * Example 1:
@@ -18,7 +19,7 @@ public class RotateArray {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        rotate(nums, 5);
+        rotate(nums, 3);
         System.out.println(Arrays.toString(nums)); // [5, 6, 7, 1, 2, 3, 4]
     }
 
@@ -32,9 +33,9 @@ public class RotateArray {
      */
     public static void rotate(int[] nums, int k) {
         k %= nums.length; // because if k > nums.length -> overflow. 3%7=3,4%7=4,15%7=1-(leftover 7+7+1)
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1); // 1234567-> 7654321
+        reverse(nums, 0, k - 1); // 7654321 -> 3456721
+        reverse(nums, k, nums.length - 1); // 3456721 -> 3456712
     }
 
     public static void reverse(int[] nums, int start, int end) {
