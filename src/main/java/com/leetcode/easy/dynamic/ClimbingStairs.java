8dc059945a10271933383d3ba42387b76826610b
Mikhail_Bobriashov <mihail.bobryashov@mail.ru>
2023-01-31 11:24:28 +0100

    #70 solution

---
+++ src/main/java/com/leetcode/easy/dynamic/ClimbingStairs.java
@@ -0,0 +1,51 @@
+package com.leetcode.easy.dynamic;
+
+/**
+ * #70
+ * <p>
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ * <p>
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ */
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        int result = new ClimbingStairs().climbStairs(5);
+
+        System.out.println(result);
+    }
+
+    // good explanation https://www.youtube.com/watch?v=Y0lT9Fck7qI&ab_channel=NeetCode
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int prev1 = 1;
+        int prev2 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int newValue = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = newValue;
+        }
+
+        return prev2;
+    }
+}
