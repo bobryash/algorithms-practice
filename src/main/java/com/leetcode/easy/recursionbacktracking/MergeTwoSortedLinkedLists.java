e0114eda36171ccfd92788e5e95657cfbb15d5cc
Mikhail_Bobriashov <bobryash@gmail.com>
2023-05-11 15:00:18 +0200

    #21 alternative solution

--- src/main/java/com/leetcode/easy/recursionbacktracking/MergeTwoSortedLinkedLists.java
+++ src/main/java/com/leetcode/easy/recursionbacktracking/MergeTwoSortedLinkedLists.java
@@ -27,7 +27,7 @@ public class MergeTwoSortedLinkedLists {
         l2.next = new ListNode(3);
         l2.next.next = new ListNode(4);
 
-        String result = mergeTwoLists(l1, l2).toString();
+        String result = mergeTwoLists2(l1, l2).toString();
 
         System.out.println(result); // [1, 1, 2, 3, 4, 4]
     }
@@ -43,6 +43,37 @@ public class MergeTwoSortedLinkedLists {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
+    }
+
+    /**
+     * The solution iteratively compares the values of the two input lists and appends the smaller value to a new list
+     * until one of the lists is exhausted. Then, it appends the remaining nodes from the non-empty list
+     * to the end of the new list.
+     *
+     * initially prehead and cur point to the same dummy object
+     * prehead is keeping all chain.
+     * cur says: now next object of the current one is THIS, and after it I become THIS (assign `next` and become next = easy chain traverse)
+     * without cur you should traverse prehead to the end each time.
+     *
+     * if use only prehead, then by prehead = prehead.next you just losing links
+     */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1); // -1 is just random number
+        ListNode cur = prehead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = l1 == null ? l2 : l1;
+        return prehead.next;
     }
 
     // ListNode definition
