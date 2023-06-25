17e5089a4336ff9059b6f7842ab701058e85ceb9
Mikhail <bobryash@gmail.com>
2023-06-25 23:12:16 +0200

    #268 solution

---
+++ src/main/java/com/leetcode/easy/MissingNumber.java
@@ -0,0 +1,51 @@
+package com.leetcode.easy;
+
+/**
+ * #268. Missing Number
+ * Easy
+ * 9.7K
+ * 3.2K
+ * Companies
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,0,1]
+ * Output: 2
+ * Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+ * 2 is the missing number in the range since it does not appear in nums.
+ * Example 2:
+ * <p>
+ * Input: nums = [0,1]
+ * Output: 2
+ * Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2].
+ * 2 is the missing number in the range since it does not appear in nums.
+ * Example 3:
+ * <p>
+ * Input: nums = [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9].
+ * 8 is the missing number in the range since it does not appear in nums.
+ */
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{3, 0, 1}));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int targetSum = (n * (n + 1)) / 2;
+
+        int sum = 0;
+
+        for (int i : nums) {
+            sum += i;
+        }
+
+        return targetSum - sum;
+    }
+}
