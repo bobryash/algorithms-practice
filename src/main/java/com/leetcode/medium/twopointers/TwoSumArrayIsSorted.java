e8870afd07c016394c96c8ae721c1de70ce0029c
Mikhail Bobriashov <bobryash@gmail.com>
2023-07-28 15:31:30 +0200

    #167 updated solution - two-pointer approach

--- src/main/java/com/leetcode/medium/twopointers/TwoSumArrayIsSorted.java
+++ src/main/java/com/leetcode/medium/twopointers/TwoSumArrayIsSorted.java
@@ -1,8 +1,6 @@
 package com.leetcode.medium.twopointers;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * #167
@@ -18,32 +16,46 @@ import java.util.Map;
  * <p>
  * Your solution must use only constant extra space.
  * <p>
+ * Example 1:
  * Input: numbers = [2,7,11,15], target = 9
  * Output: [1,2]
  * Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+ * <p>
+ * Example 2:
+ * Input: numbers = [2,3,4], target = 6
+ * Output: [1,3]
+ * Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+ * <p>
+ * Example 3:
+ * Input: numbers = [-1,0], target = -1
+ * Output: [1,2]
+ * Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
  */
 public class TwoSumArrayIsSorted {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
-
-        int[] result = twoSum(nums, target);
-
-        System.out.println(Arrays.toString(result)); // 1, 2
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9))); // [1, 2]
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 3, 4}, 6))); // [1, 2]
+        System.out.println(Arrays.toString(twoSum(new int[]{-1, 0}, -1))); // [0, 1]
     }
 
     public static int[] twoSum(int[] numbers, int target) {
-        Map<Integer, Integer> valueIndexMap = new HashMap<>();
-        for (int i = 0; i < numbers.length; i++) {
-            int v = target - numbers[i];
-            Integer r = valueIndexMap.get(v);
-            if (r != null) {
-                return new int[]{++r, ++i};
+        int left = 0;
+        int right = numbers.length - 1;
+
+        // the idea - take advantage of the fact that array is sorted.
+        // if sum of left and right values are bigger than target, then you need to decrease sum by shift right pointer
+        // if less than target, then shift left pointer
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{++left, ++right};
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
             } else {
-                valueIndexMap.put(numbers[i], i);
+                left++;
             }
         }
+
         return new int[]{-1, -1};
     }
 }
