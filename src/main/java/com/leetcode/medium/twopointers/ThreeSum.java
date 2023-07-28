9ea0cd7847de48b1bad7146b948615d4db26517c
Mikhail Bobriashov <bobryash@gmail.com>
2023-07-28 15:15:13 +0200

    #1 solution

--- src/main/java/com/leetcode/medium/twopointers/ThreeSum.java
+++ src/main/java/com/leetcode/medium/twopointers/ThreeSum.java
@@ -3,7 +3,7 @@ package com.leetcode.medium.twopointers;
 import java.util.List;
 
 /**
- * #15
+ * #15. 3Sum
  * <p>
  * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that
  * i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
