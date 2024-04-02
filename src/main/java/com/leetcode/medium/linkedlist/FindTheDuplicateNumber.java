630a3cfc9c5699a84546ecd6261f8fbead88fc2f
Mikhail Bobriashov <bobryash@gmail.com>
2024-04-02 15:36:41 +0200

    #287 solution

--- src/main/java/com/leetcode/medium/linkedlist/FindTheDuplicateNumber.java
+++ src/main/java/com/leetcode/medium/linkedlist/FindTheDuplicateNumber.java
@@ -1,5 +1,7 @@
 package com.leetcode.medium.linkedlist;
 
+import java.util.Arrays;
+
 /**
  * #287. Find the Duplicate Number
  *
@@ -23,10 +25,42 @@ package com.leetcode.medium.linkedlist;
 public class FindTheDuplicateNumber {
 
     public static void main(String[] args) {
-
+        System.out.println(findDuplicate(new int[]{1, 3, 4, 2, 2})); // 2
+        System.out.println(findDuplicate(new int[]{3, 1, 3, 4, 2})); // 3
+        System.out.println(findDuplicate(new int[]{3 ,3 ,3 ,3 ,3})); // 3
     }
 
-    public int findDuplicate(int[] nums) {
-        return 0;
+    // the idea - think of it as a linked list problem,
+    // then apply Floyd's Tortoise and Hair algorithm with a trick (2 intersections).
+    // think of array's values as pointers:
+    // index(value): 0(1) -> 1(3) -> 3(2) -> 2(4) -> [4(2) -> 2(4) -> 4(2) ->...] <- cycle in linked list!
+    public static int findDuplicate(int[] nums) {
+        // always start with 0, because 0 never is a part of a cycle
+        int slow = nums[0], fast = nums[0];
+
+        // 1st intersection: fast meets slow.
+        // why do-while:
+        // by problem description - array contains values (1..n),
+        // hence 0 will never be a part of a cycle,
+        // and hence we need to move pointers at least once
+        // to try to detect cycle.
+        // (nums[0] will never be == 0, same as other indexes)
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // 2nd intersection: new slow(start from 0) meets old slow(start from 1st intersection).
+        // just a thing you need to remember:
+        // distance from 1st intersection(slow meets fast)
+        // and distance from the start of list (slow2)
+        // are equal, and this 2nd point of intersection is always start of a cycle
+        int slow2 = nums[0];
+        while (slow2 != slow) {
+            slow2 = nums[slow2];
+            slow = nums[slow];
+        }
+
+        return slow;
     }
 }
