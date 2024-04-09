262e788a26369a6ed01263b9c45a9296fd778400
Mikhail Bobriashov <bobryash@gmail.com>
2024-04-09 23:10:02 +0200

    updates for #46, #77

--- src/main/java/com/leetcode/medium/recursionbacktracking/Combinations.java
+++ src/main/java/com/leetcode/medium/recursionbacktracking/Combinations.java
@@ -20,40 +20,54 @@ import java.util.List;
 public class Combinations {
 
     public static void main(String[] args) {
-        List<List<Integer>> result = new Combinations().combine(4, 2);
-
-        result.forEach(list -> {
-            list.forEach(System.out::print);
-            System.out.println();
-        });
+        System.out.println(combine(4, 2)); // [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
     }
 
-    // This solution uses a backtracking algorithm to generate all possible combinations of k elements from the set [1, n].
-    // The combine method is the entry point of the solution, it takes as input two integers n and k, and returns a
-    // list of all possible combinations of k elements from the set [1, n]. The backtrack method is a recursive function
-    // that generates all possible combinations by adding elements to a temporary list, recursing on the next element,
-    // and then removing the last element. It adds each combination to the result list when the temporary list has k
-    // elements.
-    //
-    // Note that this solution uses the in-built java List interface, which is a general-purpose implementation of
-    // a List. If you are working with large input values or large number of outputs, you may want to consider using a
-    // more specific implementation that is more efficient for your use case.
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), 1, n, k);
-        return result;
+    // This solution uses a backtracking algorithm to generate all possible combinations
+    // of k elements from the set [1, n].
+    // The combine method is the entry point of the solution,
+    // it takes as input two integers n and k, and returns a
+    // list of all possible combinations of k elements from the set [1, n].
+    // The backtrack method is a recursive function that generates all possible combinations by
+    // adding elements to a temporary list, recursing on the next element,
+    // and then removing the last element.
+    // It adds each combination to the result list when the temporary list has k elements.
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        backtrack(answer, tempList, n, k, 1);
+        return answer;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int start, int n, int k) {
+    // decision tree - add or not add element
+    private static void backtrack(List<List<Integer>> answer, List<Integer> tempList, int n, int k, int start) {
         if (tempList.size() == k) {
-            result.add(new ArrayList<>(tempList));
+            answer.add(new ArrayList<>(tempList));
             return;
         }
 
         for (int i = start; i <= n; i++) {
+            tempList.add(i); // add element
+            backtrack(answer, tempList, n, k, i + 1);
+            tempList.remove(tempList.size() - 1); // not to add element - undo last add
+        }
+    }
+
+    // debug
+    private static void backtrack2(List<List<Integer>> answer, List<Integer> tempList, int n, int k, int start) {
+        if (tempList.size() == k) {
+            answer.add(new ArrayList<>(tempList));
+            System.out.println("Got answer: " + tempList);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            System.out.print("Before: " + tempList);
             tempList.add(i);
-            backtrack(result, tempList, i + 1, n, k);
+            System.out.println(" After: " + tempList);
+            backtrack2(answer, tempList, n, k, i + 1);
             tempList.remove(tempList.size() - 1);
+            System.out.println("Popped last one");
         }
     }
 }
--- src/main/java/com/leetcode/medium/recursionbacktracking/Permutations.java
+++ src/main/java/com/leetcode/medium/recursionbacktracking/Permutations.java
@@ -20,29 +20,30 @@ import java.util.List;
 public class Permutations {
 
     public static void main(String[] args) {
-        List<List<Integer>> result = new Permutations().permute(new int[] {1, 2, 3});
-
-        result.forEach(list -> {
-            list.forEach(System.out::print);
-            System.out.println();
-        });
+        System.out.println(permute(new int[]{1, 2, 3})); // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        System.out.println(permute(new int[]{0, 1})); // [[0,1],[1,0]]
     }
 
-    public List<List<Integer>> permute(int[] nums) {
+    // The backtrack method is a recursive function that generates
+    // all possible permutations by adding elements to a temporary list,
+    // recursing on the next element, and then removing the last element.
+    // It adds each permutation to the result list when the temporary list
+    // has the same length as the input array.
+    //
+    // The key difference between this solution and the solution for problem #77
+    // is that this solution doesn't use a start variable, and
+    // it checks if the element is already in tempList before adding it.
+    // This is to prevent the same element from being added multiple times,
+    // since permutation involves all the possible arrangement of a set of elements,
+    // so the same element can't be used multiple times.
+    public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums);
+        List<Integer> tempList = new ArrayList<>();
+        backtrack(result, tempList, nums);
         return result;
     }
 
-    // The backtrack method is a recursive function that generates all possible permutations by adding elements to a
-    // temporary list, recursing on the next element, and then removing the last element.
-    // It adds each permutation to the result list when the temporary list has the same length as the input array.
-    //
-    //The key difference between this solution and the solution for problem #77 is that this solution doesn't use a
-    // start variable and it checks if the element is already in tempList before adding it. This is to prevent the
-    // same element from being added multiple times, since permutation involves all the possible arrangement of a set
-    // of elements, so the same element can't be used multiple times.
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+    private static void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
         if (tempList.size() == nums.length) {
             result.add(new ArrayList<>(tempList));
             return;
@@ -56,6 +57,29 @@ public class Permutations {
             tempList.add(num);
             backtrack(result, tempList, nums);
             tempList.remove(tempList.size() - 1);
+        }
+    }
+
+    // debug
+    private static void backtrack2(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            System.out.println("Got answer: " + tempList);
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int num : nums) {
+            if (tempList.contains(num)) {
+                System.out.println("Skipped: " + num);
+                continue;
+            }
+
+            System.out.print("Before: " + tempList);
+            tempList.add(num);
+            System.out.println(" After: " + tempList);
+            backtrack2(result, tempList, nums);
+            tempList.remove(tempList.size() - 1);
+            System.out.println("Popped last one");
         }
     }
 }
