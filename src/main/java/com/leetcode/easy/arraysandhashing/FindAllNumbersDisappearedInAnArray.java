1501a8652ad4fcaa92308b6caa7ec79a796af78a
Mikhail Bobriashov <bobryash@gmail.com>
2024-05-18 13:19:23 +0200

    #448 one more solution

--- src/main/java/com/leetcode/easy/arraysandhashing/FindAllNumbersDisappearedInAnArray.java
+++ src/main/java/com/leetcode/easy/arraysandhashing/FindAllNumbersDisappearedInAnArray.java
@@ -40,4 +40,31 @@ public class FindAllNumbersDisappearedInAnArray {
 
         return new ArrayList<>(set);
     }
+
+    // constant space solution
+    // the idea - values in array are guaranteed to be in 1..n and nums.length == n.
+    // so, we can consider each v value in array as an (v - 1) index in array.
+    // e.g. array [4, 4, 4, 1], v = nums[0] = 4,
+    // convert it into index: i = v - 1 (0-based arrays) = 4 - 1 = 3
+    // existence of index 3 tells us, that number 4 exists in array. (i = 3 -> v = i + 1 = 3 + 1 = 4)
+    // we need to mark that existence somehow: e.g. change the sign of the value to negative.
+    // but we might have duplicates, hence we can refer to that index several times and each time overwrite negative
+    // so each time not just take value, but take abs value, so no matter current sign, it'll be turn negative each time
+    // after marking, go through each value and check if it's non-negative - meaning i + 1 value is missing
+    public static List<Integer> findDisappearedNumbers2(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            // map nums[i] to index
+            int index = Math.abs(nums[i]) - 1;
+            nums[index] = -Math.abs(nums[index]);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
 }
